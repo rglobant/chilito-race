d15076030b55999223f981cafd94ab694c46f468
Rafael Alejandro Garduno Escoto <rafael.garduno@globant.com>
2024-10-07 16:01:27 -0600

    Login Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">Hello World!</h1>
-    </div>
+    // <div className="h-screen flex justify-center items-center bg-gray-100">
+    //   <h1 className="text-3xl font-bold text-blue-500">Hello World!</h1>
+    // </div>
+
+    <>
+    <Login />
+    </>
   );
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,51 @@
 import React from "react";
 
 const Login: React.FC = () => {
-
   return (
-    <div>
-      Login Page
+    <div className="animate-pulse min-h-screen bg-slate-200 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
+      <span className="border text-4xl text-yellow-800 px-6 pt-10 pb-8 bg-white w-1/2 max-w-md mx-auto rounded-t-md sm:px-10">
+        Sign in
+      </span>
+      <div className="border relative px-4 pt-7 pb-8 bg-white shadow-xl w-1/2 max-w-md mx-auto sm:px-10 rounded-b-md">
+        <form action="">
+          <label className="block">Username or Email</label>
+          <input
+            type="email"
+            className="border w-full h-10 px-3 mb-5 rounded-md"
+            placeholder="Username or Email"
+          />
+          <label className="block">Password</label>
+          <input
+            type="password"
+            className="border w-full h-10 px-3 mb-5 rounded-md"
+            placeholder="password"
+          />
+          <div className="flex items-start">
+            <div className="flex items-start">
+              <div className="flex items-center">
+                <input
+                  id="remember"
+                  aria-describedby="remember"
+                  type="checkbox"
+                  className="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                />
+              </div>
+              <div className="text-sm ml-3">
+                <label className="font-medium text-gray-900">Remember me</label>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500"
+            >
+              Lost Password?
+            </a>
+          </div>
+          <button className="mt-5 bg-green-500 hover:bg-blue-700 shadow-xl text-white uppercase text-sm font-semibold px-14 py-3 rounded">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
